f9e71b2f0bcc16f0ef31895ce317805c47dc3c31
shrijoshi25 <shrikantj2504@gmail.com>
2025-10-27 10:26:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Initialize AOS (Animate On Scroll)
+AOS.init({
+  duration: 800,  // Animation duration in ms
+  easing: 'ease-in-out',
+  once: true,     // Animation only once
+  mirror: false,  // Disable repeating animation
+});
